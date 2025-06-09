d069378eaea70bdeb21630a8b60fdeb075b48f7e
Jesse Greathouse <jessegreathouse@gmail.com>
2025-06-09 05:31:13 +0000

    Updated bin/modules/WPControl/Configure.pm module and configuration file templates

--- bin/modules/WPControl/Configure.pm
+++ bin/modules/WPControl/Configure.pm
@@ -1,142 +1,285 @@
 #!/usr/bin/perl
 
 package WPControl::Configure;
+
 use strict;
-use File::Basename;
-use Cwd qw(getcwd abs_path);
+use warnings;
 use Exporter 'import';
-use Data::Dumper;
-use Scalar::Util qw(looks_like_number);
-use Term::Prompt;
-use Term::Prompt qw(termwrap);
+use File::Basename;
+use Cwd qw(abs_path);
+use Term::Prompt qw(prompt termwrap);
 use Term::ANSIScreen qw(cls);
-use lib(dirname(abs_path(__FILE__))  . "/../modules");
-use WPControl::Config qw(get_configuration save_configuration);
-use WPControl::Utility qw(splash);
+use lib(dirname(abs_path(__FILE__)) . "/../modules");
+use WPControl::Config qw(get_configuration save_configuration write_config_file);
+use WPControl::Utility qw(splash generate_rand_str);
+use WPControl::RefreshKeysAndSalts qw(refresh_keys_and_salts);
 
-our @EXPORT_OK = qw(configure);
+our @EXPORT_OK = qw(configure configure_help);
 
-my $os = get_operating_system();
-my $osModule = 'WPControl::Configure::' . $os;
-eval "use $osModule qw(
-    get_user_input
-)";
-warn $@ if $@; # handle exception
+warn $@ if $@;
 
-my $user = $ENV{'LOGNAME'} || $ENV{'USER'} || getpwuid($<);
-my $bin = abs_path(dirname(__FILE__) . '/../../');
-my $applicationRoot = abs_path(dirname($bin));
-my $etc = $applicationRoot . '/etc';
-my $opt = $applicationRoot . '/opt';
-my $src = $applicationRoot . '/src';
-my $web = $applicationRoot . '/web';
-my $passwordMatchAttempts = 0;
-my $adminPasswordConfirm = '';
+# ------------------------
+# Define Application Paths
+# ------------------------
+
+my $binDir          = abs_path(dirname(__FILE__) . '/../../');
+my $applicationRoot = abs_path(dirname($binDir));
+my $etcDir          = "$applicationRoot/etc";
+my $optDir          = "$applicationRoot/opt";
+my $varDir          = "$applicationRoot/var";
+my $webDir          = "$applicationRoot/web";
+my $srcDir          = "$applicationRoot/src";
+my $tmpDir          = "$applicationRoot/tmp";
+my $logDir          = "$varDir/log";
+my $cacheDir        = "$varDir/cache";
+
+my $secret = generate_rand_str();
+
+# Default Supervisor control ports
+my $supervisorPort = 5962;
+
+# Files
+my $sslCertificate  = "$etcDir/ssl/certs/wp-control.cert";
+my $sslKey          = "$etcDir/ssl/private/wp-control.key";
+my $errorLog        = "$logDir/error.log";
+my $keysAndSalts    = "$varDir/keys/wordpress-keys-and-salts.php";
+
+# ------------------------
+# Load and Define Config
+# ------------------------
+
 my %cfg = get_configuration();
 
-1;
+# List of configuration files to be written
+my %config_files = (
+    php_ini             => ["$etcDir/php/php.dist.ini",                         "$etcDir/php/php.ini"],
+    php_fpm             => ["$etcDir/php-fpm.d/php-fpm.dist.conf",              "$etcDir/php-fpm.d/php-fpm.conf"],
+    force_ssl           => ["$etcDir/nginx/force-ssl.dist.conf",                "$etcDir/nginx/force-ssl.conf"],
+    ssl_params          => ["$etcDir/nginx/ssl-params.dist.conf",               "$etcDir/nginx/ssl-params.conf"],
+    nginx               => ["$etcDir/nginx/nginx.dist.conf",                    "$etcDir/nginx/nginx.conf"],
+    wordpress_cfg       => ["$etcDir/wordpress/wp-config.php",                  "$webDir/wp-config.php"],
+    wordpress_env       => ["$etcDir/wordpress/env.php",                        "$webDir/env.php"],
+    supervisord         => ["$etcDir/supervisor/conf.d/supervisord.conf.dist",  "$etcDir/supervisor/conf.d/supervisord.conf"],
+);
 
-# ====================================
-#    Subroutines below this point
-# ====================================
+# Default values
+my %defaults = (
+  nginx => {
+    PORT         => '8181',
+    SSL_CERT     => $sslCertificate,
+    SSL_KEY      => $sslKey,
+    IS_SSL       => 'false',
+    MAIN_DOMAIN  => 'example.com',
+    SITE_DOMAINS => 'www.example.com',
+  },
+  wordpress => {
+      SITE_TITLE     => 'Just Another WordPress Site',
+      DB_HOST        => '127.0.0.1',
+      DB_PORT        => '3306',
+      DEBUG          => 'true',
+  },
+  supervisors => {
+      SUPERVISORCTL_USER => $ENV{"LOGNAME"},
+  },
+);
 
-# Trim the whitespace from a string.
-sub  trim { my $s = shift; $s =~ s/^\s+|\s+$//g; return $s };
+# ================================
+#       PUBLIC ENTRYPOINTS
+# ================================
 
-# Returns string associated with operating system.
-sub get_operating_system {
-    return 'Ubuntu';
+sub configure_help {
+    print <<'EOF';
+Usage: configure [--option]
+
+Sets up the wp-control configuration system. By default, the script runs in interactive mode.
+
+Examples:
+  configure                   # Run interactive configuration
+  configure --non-interactive # Use default or pre-defined values
+
+Available options:
+  --non-interactive   Skip all interactive prompts
+  help                Show this help message
+EOF
 }
 
-# Performs the install routine.
 sub configure {
-    cls();
-    splash();
+    my ($interactive) = @_;
+    $interactive = 1 unless defined $interactive;
 
-    print (''."\n");
-    print ('================================================================='."\n");
-    print (' This will create your site\'s run script'."\n");
-    print ('================================================================='."\n");
-    print (''."\n");
+    if ($interactive) {
+        cls();
+        splash();
+        print "\n=================================================================\n";
+        print " This will configure your WordPress hosting environment\n";
+        print "=================================================================\n\n";
+        merge_defaults();
+        prompt_user_input();
+    }
 
-    request_user_input();
-
+    assign_dynamic_config();
     save_configuration(%cfg);
-}
 
-# Runs the user through a series of setup config questions.
-# Confirms the answers.
-# Returns Hash Table
-sub request_user_input {
-    # Save the input variables to the configuration.
-    $cfg{'meta'}{'site_name'} = '';
-    $cfg{'redis'}{'redis_host'} = '';
-    $cfg{'nginx'}{'port'} = '';
-    $cfg{'nginx'}{'ssl_pair'} = '';
-    $cfg{'nginx'}{'ssl_cert'} = '';
-    $cfg{'nginx'}{'ssl_key'} = '';
-    $cfg{'nginx'}{'main_domain'} = '';
-    $cfg{'nginx'}{'site_domains'} = '';
-    $cfg{'wordpress'}{'admin_email'} = '';
-    $cfg{'wordpress'}{'admin_password'} = '';
-    $cfg{'wordpress'}{'site_title'} = '';
-    $cfg{'wordpress'}{'db_host'} = '';
-    $cfg{'wordpress'}{'db_name'} = '';
-    $cfg{'wordpress'}{'db_user'} = '';
-    $cfg{'wordpress'}{'db_password'} = '';
-    $cfg{'wordpress'}{'db_port'} = '';
-    $cfg{'wordpress'}{'replace_keys_and_salts'} = '';
-    $cfg{'wordpress'}{'debug'} = '';
+    # Refreshes the cfg variable with exactly what was just written to the file.
+    my %liveCfg = get_configuration();
 
-    # SITE_NAME
-    if ($cfg{'meta'}{'site_name'} eq '') {
-        input_site_name();
+    # Write configuration files
+    foreach my $key (keys %config_files) {
+      write_config(@{$config_files{$key}}, \%liveCfg);
     }
 
-    # ADMIN_EMAIL
-    if ($cfg{'wordpress'}{'admin_email'} eq '') {
-        input_admin_email();
-    }
+    if ($interactive) {
+        prompt_refresh_keys_and_salts();
+        prompt_admin_password();
+    } else {
+        # run keys and salts if it doesn't exist:
+        -e $keysAndSalts or refresh_keys_and_salts();
 
-    # ADMIN_PASSWORD
-    if ($cfg{'wordpress'}{'admin_password'} eq '') {
-        input_admin_password();
-    }
-
-    if ($cfg{'wordpress'}{'site_title'} eq '') {
-        input_site_title();
+        print "\nConfiguration completed in non-interactive mode.\n";
+        print "Note: If this is a fresh install, be sure to manually run the following commands as needed:\n";
+        print "  bin/refresh-wp-keys-and-salts  # Run database migrations\n";
     }
 }
 
-sub input_site_name {
-    $cfg{'meta'}{'site_name'} = prompt('x', 'Profile name of this site:', '', '');
+# ================================
+#        SUBROUTINES
+# ================================
+
+# Writes a configuration file from its template.
+sub write_config {
+    my ($distFile, $outFile, $config_ref) = @_;
+    return unless -e $distFile;
+    write_config_file($distFile, $outFile, %$config_ref);
 }
 
-sub input_admin_email {
-    $cfg{'wordpress'}{'admin_email'} = prompt('x', 'Admin email address of your site:', '', '');
-}
-
-sub input_site_title {
-    $cfg{'wordpress'}{'site_title'} = prompt('x', 'WordPress Title of your site:', '', 'Just Another WordPress Site');
-}
-
-sub input_admin_password {
-    if ($cfg{'wordpress'}{'admin_password'} eq '' or ($adminPasswordConfirm ne $cfg{'wordpress'}{'admin_password'})) {
-        ++$passwordMatchAttempts;
-
-        if ($passwordMatchAttempts > 3) {
-            die ("Too many failed Admin Password Confirmations. Please run the script again to configure.\n");
+sub merge_defaults {
+    foreach my $domain (keys %defaults) {
+        foreach my $key (keys %{$defaults{$domain}}) {
+            $cfg{$domain}{$key} //= $defaults{$domain}{$key};
         }
-
-        if ($passwordMatchAttempts > 1) {
-            print termwrap("The Admin Password did not match with the confirmation.\nPlease attempt to provide the Admin Password Again.\n");
-        }
-
-        $cfg{'wordpress'}{'admin_password'} = prompt('p', 'Admin password of your site:', '', '');
-        print "\n";
-        $adminPasswordConfirm = prompt('p', 'Confirm the Admin password of your site:', '', '');
-        print "\n";
-
-        input_admin_password();
     }
 }
+
+sub assign_dynamic_config {
+    # Assign essential directory paths
+    $cfg{nginx}{WEB}          //= $webDir;
+    $cfg{nginx}{VAR}          //= $varDir;
+    $cfg{nginx}{LOG}          //= $logDir;
+    $cfg{nginx}{ETC}          //= $etcDir;
+    $cfg{nginx}{OPT}          //= $optDir;
+    $cfg{nginx}{SRC}          //= $srcDir;
+    $cfg{nginx}{TMP}          //= $tmpDir;
+    $cfg{nginx}{BIN}          //= $binDir;
+    $cfg{nginx}{CACHE}        //= $cacheDir;
+    $cfg{nginx}{DIR}          //= $applicationRoot;
+
+    $cfg{nginx}{USER} //= $ENV{"LOGNAME"};
+    $cfg{nginx}{SESSION_SECRET} //= $secret;
+
+    if ($cfg{nginx}{IS_SSL} eq 'true') {
+        $cfg{nginx}{SSL_CERT_LINE} = "ssl_certificate $cfg{nginx}{SSL_CERT};";
+        $cfg{nginx}{SSL_KEY_LINE}  = "ssl_certificate_key $cfg{nginx}{SSL_KEY};";
+        $cfg{nginx}{INCLUDE_FORCE_SSL} = "include $etcDir/nginx/force-ssl.conf;";
+        $cfg{nginx}{SSL} = "ssl";
+    } else {
+        $cfg{nginx}{SSL_CERT_LINE} = "";
+        $cfg{nginx}{SSL_KEY_LINE}  = "";
+        $cfg{nginx}{INCLUDE_FORCE_SSL} = "";
+        $cfg{nginx}{SSL} = "";
+    }
+
+    # Assign dynamically generated values that are not part of %defaults
+    $cfg{supervisord}{SUPERVISORCTL_USER} //= $ENV{"LOGNAME"};
+    $cfg{supervisord}{SUPERVISORCTL_SECRET} //= $secret;
+    $cfg{supervisord}{SUPERVISORCTL_PORT} //= $supervisorPort;
+}
+
+sub prompt_user_input {
+      my @fields = (
+        ['meta',      'SITE_NAME',     'Site Label'],
+        ['wordpress', 'ADMIN_EMAIL',   'Admin Email Address'],
+        ['wordpress', 'SITE_TITLE',    'Site Title'],
+        ['wordpress', 'DB_HOST',       'Database Host'],
+        ['wordpress', 'DB_NAME',       'Database Name'],
+        ['wordpress', 'DB_USER',       'Database Username'],
+        ['wordpress', 'DB_PASSWORD',   'Database Password'],
+        ['wordpress', 'DB_PORT',       'Database Port'],
+        ['wordpress', 'DEBUG',         'Enable Debugging'],
+        ['nginx',     'MAIN_DOMAIN',   'Primary Domain Name'],
+        ['nginx',     'SITE_DOMAINS',  'Additional Domains (space-separated)'],
+        ['nginx',     'IS_SSL',        'Enable SSL (HTTPS)'],
+        ['nginx',     'SSL_CERT',      'SSL Certificate Path (if using HTTPS)'],
+        ['nginx',     'SSL_KEY',       'SSL Key Path (if using HTTPS)'],
+        ['nginx',     'PORT',          'Web Server Port'],
+    );
+
+    foreach my $field (@fields) {
+        my ($domain, $key, $label) = @$field;
+        if ($key =~ /DEBUG|IS_SSL/) {
+            $cfg{$domain}{$key} = prompt_boolean($cfg{$domain}{$key}, $label);
+        } elsif ($key =~ /PORT/) {
+            $cfg{$domain}{$key} = prompt_integer($cfg{$domain}{$key}, $label);
+        } else {
+            $cfg{$domain}{$key} = prompt('x', "$label:", '', $cfg{$domain}{$key});
+        }
+    }
+}
+
+sub prompt_boolean {
+    my ($default, $label) = @_;
+    $default //= 'false';
+    my $prompt_val = ($default eq 'true') ? 'y' : 'n';
+    return prompt('y', "$label", '', $prompt_val) ? 'true' : 'false';
+}
+
+sub prompt_integer {
+    my ($default, $label) = @_;
+    while (1) {
+        my $val = prompt('x', "$label (integer):", '', $default);
+        return $val if $val =~ /^\d+$/;
+        print "Invalid input. Please enter an integer.\n";
+    }
+}
+
+# Displays a prompt to refresh WordPress Keys and Salts file.
+sub prompt_refresh_keys_and_salts {
+
+    if (!-e $keysAndSalts) {
+        refresh_keys_and_salts();
+        return;
+    }
+
+    print "\n=================================================================\n";
+    print " Refresh WordPress Keys and Salts\n";
+    print "=================================================================\n\n";
+
+    print "Refresh WordPress Keys and Salts?\n";
+    print "This will cancel all existing/active users sessions.\n\n";
+    print "You can also run this manually later using: bin/refresh-wp-keys-and-salts\n\n";
+
+    my $answer = prompt('y', "Refresh WordPress Keys and Salts?", '', "n");
+
+    if ($answer eq 1) {
+        refresh_keys_and_salts();
+    } else {
+        print "\n";
+    }
+}
+
+# Optionally prompt to set the admin password post-configuration
+sub prompt_admin_password {
+    print "\n=================================================================\n";
+    print " Admin User Password Change\n";
+    print "=================================================================\n\n";
+
+    print "Change the WordPress admin account password on demand.\n";
+    print "You can provide it or skip this step.\n\n";
+
+    my $answer = prompt('y', "Set the admin password now?", '', 'n');
+
+    if ($answer eq 'y') {
+      #TODO: Change admin passowrd.
+    }
+}
+
+1;
--- bin/modules/WPControl/Utility.pm
+++ bin/modules/WPControl/Utility.pm
@@ -1,18 +1,227 @@
 #!/usr/bin/perl
 
 package WPControl::Utility;
+
 use strict;
 use Exporter 'import';
+use Errno;
+use POSIX 'ceil';
 
 our @EXPORT_OK = qw(
-    splash
+  command_result
+  get_operating_system
+  read_file
+  write_file
+  trim
+  splash
+  str_replace_in_file
+  generate_rand_str
+  is_pid_running
 );
-
-1;
 
 # ====================================
 #    Subroutines below this point
 # ====================================
+
+# Trim the whitespace from a string.
+sub trim {
+    my $s = shift;
+    $s =~ s/^\s+|\s+$//g;
+    return $s;
+}
+
+# Returns string associated with operating system.
+sub get_operating_system {
+    my %osNames = (
+        MSWin32 => 'Win32',
+        NetWare => 'Win32',
+        symbian => 'Win32',
+        darwin  => 'MacOS'
+    );
+
+    # Check for Linux-based OS and delegate to a separate function
+    if ($^O eq 'linux') {
+        return get_linux_distribution();
+    }
+
+    # If $^O is not found in the hash, die with an error message
+    die "Unsupported operating system: $^O\n" unless exists $osNames{$^O};
+
+    return $osNames{$^O};
+}
+
+# Detects the Linux distribution.
+sub get_linux_distribution {
+    # Arrays for different types of distribution identification
+    my @os_release_dists = (
+        { pattern => 'centos',          name => 'CentOS' },
+        { pattern => 'ubuntu',          name => 'Ubuntu' },
+        { pattern => 'fedora',          name => 'Fedora' },
+        { pattern => 'debian',          name => 'Debian' },
+        { pattern => 'opensuse',        name => 'OpenSUSE' },
+        { pattern => 'arch',            name => 'Arch' },
+        { pattern => 'alpine',          name => 'Alpine' },
+        { pattern => 'gentoo',          name => 'Gentoo' },
+        { pattern => 'openmandriva',    name => 'OpenMandriva' },
+    );
+
+    # Check /etc/os-release first (most modern distros)
+    if (open my $fh, '<', '/etc/os-release') {
+        while (my $line = <$fh>) {
+            foreach my $dist (@os_release_dists) {
+                if ($line =~ /^ID=$dist->{pattern}/) {
+                    return $dist->{name};
+                }
+            }
+        }
+    }
+
+    # Fallback to other common files
+    if (-e '/etc/lsb-release') {
+        if (open my $fh, '<', '/etc/lsb-release') {
+            while (my $line = <$fh>) {
+                foreach my $dist (@os_release_dists) {
+                    if ($line =~ /DISTRIB_ID=$dist->{name}/i) {
+                        return $dist->{name};
+                    }
+                }
+            }
+        }
+    }
+
+    if (-e '/etc/redhat-release') {
+        if (open my $fh, '<', '/etc/redhat-release') {
+            while (my $line = <$fh>) {
+                foreach my $dist (@os_release_dists) {
+                    if ($line =~ /$dist->{name}/i) {
+                        return $dist->{name};
+                    }
+                }
+            }
+        }
+    }
+
+    # Check /etc/debian_version for Debian-based distros
+    if (-e '/etc/debian_version') {
+        return 'Debian';
+    }
+
+    # Use uname as a last resort (generic fallback)
+    my $uname = `uname -a`;
+    foreach my $dist (@os_release_dists) {
+        if ($uname =~ /$dist->{name}/i) {
+            return $dist->{name};
+        }
+    }
+
+    # If no distribution was found, throw an error
+    die "Unable to determine Linux distribution.\n";
+}
+
+# Replaces all occurrences of a string with another string inside a file.
+sub str_replace_in_file {
+    my ($string, $replacement, $file) = @_;
+    my $data = read_file($file);
+    $data =~ s/\Q$string/$replacement/g;
+    write_file($file, $data);
+}
+
+# Reads the contents of a UTF-8 encoded file and returns it as a string.
+sub read_file {
+    my ($filename) = @_;
+
+    open my $in, '<:encoding(UTF-8)', $filename or die "Could not open '$filename' for reading $!";
+    local $/ = undef;
+    my $all = <$in>;
+    close $in;
+
+    return $all;
+}
+
+# Writes a string to a UTF-8 encoded file.
+sub write_file {
+    my ($filename, $content) = @_;
+
+    open my $out, '>:encoding(UTF-8)', $filename or die "Could not open '$filename' for writing $!";
+    print $out $content;
+    close $out;
+
+    return;
+}
+
+# Handles system command exit status and prints a result message.
+sub command_result {
+    my ($exit, $err, $operation_str, @cmd) = @_;
+
+    if ($exit == -1) {
+        print "Failed to execute command: $err\n";
+        print "Command: @cmd\n" if @cmd;
+        exit 1;
+    }
+    elsif ($exit & 127) {
+        my $signal = $exit & 127;
+        my $coredump = ($exit & 128) ? 'with' : 'without';
+        print "Command died with signal $signal ($coredump coredump).\n";
+        print "Command: @cmd\n" if @cmd;
+        exit 1;
+    }
+    else {
+        my $code = $exit >> 8;
+        if ($code != 0) {
+            print "Command exited with non-zero status $code.\n";
+            print "Command: @cmd\n" if @cmd;
+            exit $code;
+        }
+        else {
+            print "$operation_str success!\n";
+        }
+    }
+}
+
+# Generates a random hexadecimal string of a given length (default: 64 characters).
+sub generate_rand_str {
+    my ($length) = @_;
+    $length //= 64;
+
+    # Each byte = 2 hex chars, so we need ceil(length / 2) bytes
+    my $bytes_needed = ceil($length / 2);
+
+    open my $urandom, '<:raw', '/dev/urandom' or die "Can't open /dev/urandom: $!";
+    read($urandom, my $raw, $bytes_needed) == $bytes_needed or die "Failed to read enough bytes from /dev/urandom";
+    close $urandom;
+
+    my $hex = uc unpack('H*', $raw);   # Convert to uppercase hex
+    return substr($hex, 0, $length);   # Truncate length
+}
+
+# Checks whether a PID from a file is currently running.
+sub is_pid_running {
+    my ($pidFile) = @_;
+
+    open my $fh, '<', $pidFile or die "Can't open $pidFile: $!";
+    my $pid = do { local $/; <$fh> };
+    close $fh;
+
+    # Strip whitespace/newlines
+    $pid =~ s/^\s+|\s+$//g;
+
+    # Validate PID is numeric
+    return 0 unless defined $pid && $pid =~ /^\d+$/;
+
+    my %dispatch = (
+        success     => sub { return 1 },
+        no_perm     => sub { return 1 },
+        not_found   => sub { return 0 },
+    );
+
+    my $result = kill(0, $pid);
+
+    return $dispatch{
+                    $result             ? 'success' :
+                    $! == Errno::EPERM  ? 'no_perm' :
+                                        'not_found'
+    }->();
+}
 
 # Prints a spash screen message.
 sub splash() {
@@ -37,3 +246,5 @@ sub splash() {
   print ('+--------------------------------------------------------------------------------------+'."\n");
   print (''."\n");
 }
+
+1;
